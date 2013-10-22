f62221ca25c9c95fd8279921a7ac1202b07673a5
Thanaphol Headkhunthod <robotnoy.sut@gmail.com>
2013-10-22 11:51:26 -0700

    A    testAnnotationParser A    testAnnotationParser/.project A    testAnnotationParser/src A    testAnnotationParser/src/controller A    testAnnotationParser/src/controller/Demo.java A    testAnnotationParser/src/annotation A    testAnnotationParser/src/annotation/TestAnnotationParser.java A    testAnnotationParser/src/annotation/Copyright.java A    testAnnotationParser/src/annotation/Annotated.java A    testAnnotationParser/src/annotation/Test.java A    testAnnotationParser/bin A    testAnnotationParser/bin/controller AM   testAnnotationParser/bin/controller/Demo.class A    testAnnotationParser/bin/annotation AM   testAnnotationParser/bin/annotation/TestAnnotationParser.class AM   testAnnotationParser/bin/annotation/Copyright.class AM   testAnnotationParser/bin/annotation/Annotated.class AM   testAnnotationParser/bin/annotation/Test.class A    testAnnotationParser/.settings A    testAnnotationParser/.settings/org.eclipse.jdt.core.prefs A    testAnnotationParser/.classpath

---
+++ testAnnotationParser/src/annotation/Annotated.java
@@ -0,0 +1,10 @@
+package annotation;
+
+
+public class Annotated {
+	
+    @Test(info = "AWESOME")
+    public void foo(String myParam) {
+        System.out.println("This is " + myParam);
+    }
+}
